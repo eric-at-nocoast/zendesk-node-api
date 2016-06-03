b35dc8e0d4e68d6b6eaedc5dccdc18bbfd2a404e
Adam Gray <adam@adamgray.so>
2016-06-03 12:51:28 +0100

    Remove config from index

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var Zendesk = require('./src/zendesk.js');
-
-var zendesk = new Zendesk({
-  url: 'https://z3n3310.zendesk.com',
-  email: 'agray@zendesk.com',
-  token: 'hyjCGMknQUmlvOWChLoitKZFUVdsnfSUBn4DnxPd'
-});
+module.exports = require('./src/zendesk.js');
