2f722a694284926440d12870d663f8a568625ba3
Adam Gray <adam@adamgray.so>
2016-06-08 22:22:38 +0100

    Add more ticket tests

--- test/test.js
+++ test/test.js
@@ -1,26 +1,51 @@
 var should = require('chai').should();
+var expect = require('chai').expect;
 
 var Zendesk = require('../index.js');
-
 var zendesk = new Zendesk({
   url: process.env.ZENDESK_URL,
   email: process.env.ZENDESK_EMAIL,
   token: process.env.ZENDESK_API_KEY
 });
 
-var TEST_TICKET_ID = 212;
+var TICKET_ID = 212;
 
 describe('Zendesk', function(){
   describe('tickets', function(){
-    it('should get all tickets', function(){
+    it('should get all tickets', function(done){
       zendesk.tickets.list().then(function(tickets){
-        expect(tickets).to.be.instanceOf(Object);
+        expect(tickets).to.exist;
+        done();
       });
     });
 
-    it('should get ticket by id', function(){
-      zendesk.tickets.show(TEST_TICKET_ID).then(function(ticket){
-        expect(ticket).to.be.instanceOf(Object);
+    it('should show a ticket by id', function(done){
+      zendesk.tickets.show(TICKET_ID).then(function(ticket){
+        expect(ticket).to.exist;
+        done();
+      });
+    });
+
+    it('should create a ticket', function(done){
+      zendesk.tickets.create({
+        subject: 'A new ticket',
+        comment: {
+          body: 'A ticket created with zendesk-node-api'
+        }
+      }).then(function(data){
+        expect(data).to.exist;
+        expect(data.ticket.subject).to.equal('A new ticket');
+        done();
+      });
+    });
+
+    it('should update a ticket', function(done){
+      zendesk.tickets.update(TICKET_ID, {
+        subject: 'Updated ticket'
+      }).then(function(data){
+        expect(data).to.exist;
+        expect(data.ticket.subject).to.equal('Updated ticket');
+        done();
       });
     });
   });
